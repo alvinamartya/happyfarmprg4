18756f0d2eba326ee3ac3114485668285b2da1db
Alvin Amartya <alvinamartya1@gmail.com>
2021-01-11 14:45:25 +0700

    Update login

---
+++ HappyFarmProject/HappyFarmProjectWebAdmin/Controllers/MarketingAdmin/MarketingAdminPromoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HappyFarmProjectWebAdmin.Controllers
+{
+    public class MarketingAdminPromoController : Controller
+    {
+        // GET: MarketingAdminPromo
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ HappyFarmProject/HappyFarmProjectWebAdmin/Controllers/ProductionAdmin/ProductionAdminGoodsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HappyFarmProjectWebAdmin.Controllers
+{
+    public class ProductionAdminGoodsController : Controller
+    {
+        // GET: ProductionAdminGoods
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
