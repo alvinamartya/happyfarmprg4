c6b3163c230136342d39c2e1ae3d84c3e68bb1d3
Alvin Amartya <alvinamartya1@gmail.com>
2021-01-14 21:32:18 +0700

    merging

---
+++ HappyFarmProject/HappyFarmProjectWebAdmin/Models/Request/AddPromoRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HappyFarmProjectWebAdmin.Models
+{
+    public class AddPromoRequest
+    {
+        public int CreatedBy { get; set; }
+        public string Name { get; set; }
+        public string ImagePath { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string IsFreeDelivery { get; set; }
+        public float Discount { get; set; }
+        public decimal MinTransaction { get; set; }
+        public decimal MaxDiscount { get; set; }
+    }
+}
