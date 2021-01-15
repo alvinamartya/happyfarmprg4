13d8a2017f1109d3cb108d529c3a103ee5c678a5
Alvin Amartya <alvinamartya1@gmail.com>
2021-01-15 13:24:27 +0700

    Update Region API Manager

--- HappyFarmProject/HappyFarmProjectAPI/Controllers/Manager/ManagerRegionController.cs
+++ HappyFarmProject/HappyFarmProjectAPI/Controllers/Manager/ManagerRegionController.cs
@@ -356,6 +356,57 @@ namespace HappyFarmProjectAPI.Controllers
                 return InternalServerError(ex);
             }
         }
+
+        /// <summary>
+        /// To get regions
+        /// </summary>
+        /// <param name="getListData"></param>
+        /// <returns></returns>
+        [Route("api/v1/Manager/Region")]
+        [HttpGet]
+        public async Task<IHttpActionResult> GetRegions()
+        {
+            try
+            {
+                // validate token
+                if (tokenLogic.ValidateTokenInHeader(Request, "Manager"))
+                {
+                    List<Region> regions = await Task.Run(() => repo.GetRegions());
+
+                    // response success
+                    var response = new ResponseWithData<Object>()
+                    {
+                        StatusCode = HttpStatusCode.OK,
+                        Message = "Berhasil",
+                        Data = regions
+                            .Select(x => new
+                            {
+                                x.Id,
+                                x.Name
+                            })
+                            .ToList(),
+                    };
+
+                    return Ok(response);
+                }
+                else
+                {
+                    // unauthorized
+                    var unAuthorizedResponse = new ResponseWithoutData()
+                    {
+                        StatusCode = HttpStatusCode.Unauthorized,
+                        Message = "Anda tidak memiliki hak akses"
+                    };
+
+                    return Ok(unAuthorizedResponse);
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.Write("Error: " + ex.Message);
+                return InternalServerError(ex);
+            }
+        }
         #endregion
     }
 }
