bfb031a4bcf8a88979b5c97cbf38f61a45b384ab
syamsulr17 <64457807+syamsulr17@users.noreply.github.com>
2021-01-20 14:03:08 +0700

    tes

---
+++ HappyFarmProject/HappyFarmProjectAPI/Controllers/BusinessLogic/CustomerFeedbackLogic.cs
@@ -0,0 +1,99 @@
+﻿using HappyFarmProjectAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+namespace HappyFarmProjectAPI.Controllers.BusinessLogic
+{
+    public class CustomerFeedbackLogic
+    {
+        /// <summary>
+        /// Check Authorization
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        private ResponseModel checkAuthorization(int id, HttpStatusCode responseSuccess)
+        {
+            using (HappyFarmPRG4Entities db = new HappyFarmPRG4Entities())
+            {
+                var employee = db.Employees.Where(x => x.Id == id && x.RowStatus == "A").FirstOrDefault();
+                if (employee != null)
+                {
+                    if (employee.UserLogin.Role.Name != "Super Admin" && employee.UserLogin.Role.Name != "Customer Service")
+                    {
+                        // unauthroized
+                        return new ResponseModel()
+                        {
+                            StatusCode = HttpStatusCode.Unauthorized,
+                            Message = "Anda tidak memiliki hak akses"
+                        };
+                    }
+                    else
+                    {
+                        // return ok
+                        return new ResponseModel()
+                        {
+                            Message = "Berhasil",
+                            StatusCode = responseSuccess
+                        };
+                    }
+                }
+                else
+                {
+                    // employee not found
+                    return new ResponseModel()
+                    {
+                        Message = "Data karyawan tidak ditemukan",
+                        StatusCode = HttpStatusCode.BadRequest
+                    };
+                }
+            }
+        }
+
+        /// <summary>
+        /// Validate data for getting customer feedback by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public ResponseModel GetCustomerFeedbackById(int id, string role)
+        {
+            using (HappyFarmPRG4Entities db = new HappyFarmPRG4Entities())
+            {
+                // get region
+                var customerFeedbacks = db.CustomerFeedbacks.Where(x => x.Id == id).FirstOrDefault();
+                if (customerFeedbacks != null)
+                {
+                    if (role != "Super Admin" && role != "Customer Service")
+                    {
+                        // unauthorized
+                        return new ResponseModel()
+                        {
+                            StatusCode = HttpStatusCode.Unauthorized,
+                            Message = "Anda tidak memiliki hak akses"
+                        };
+                    }
+                    else
+                    {
+                        // return ok
+                        return new ResponseModel()
+                        {
+                            Message = "Berhasil",
+                            StatusCode = HttpStatusCode.OK
+                        };
+                    }
+                }
+                else
+                {
+                    // data is not found
+                    return new ResponseModel()
+                    {
+                        Message = "Customer Feedback tidak tersedia",
+                        StatusCode = HttpStatusCode.BadRequest
+                    };
+                }
+            }
+        }
+    }
+}
---
+++ HappyFarmProject/HappyFarmProjectAPI/Controllers/Repository/CustomerFeedbackRepository.cs
@@ -0,0 +1,62 @@
+﻿using HappyFarmProjectAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HappyFarmProjectAPI.Controllers.Repository
+{
+    public class CustomerFeedbackRepository
+    {
+        /// <summary>
+        /// Get banner with paging
+        /// </summary>
+        /// <param name="currentPage"></param>
+        /// <param name="limitPage"></param>
+        /// <param name="search"></param>
+        /// <returns></returns>
+        public ResponsePagingModel<List<CustomerFeedback>> GetCustomerFeedbacks(int currentPage, int limitPage, string search)
+        {
+            using (HappyFarmPRG4Entities db = new HappyFarmPRG4Entities())
+            {
+                // get customerFeedbacks
+                var customerFeedbacks = db.CustomerFeedbacks.ToList();
+
+                // get total customerFeedbacks
+                var totalPages = Math.Ceiling((decimal)customerFeedbacks.Count / limitPage);
+
+                // return customerFeedbacks
+                return new ResponsePagingModel<List<CustomerFeedback>>()
+                {
+                    Data = customerFeedbacks,
+                    CurrentPage = currentPage,
+                    TotalPage = (int)totalPages
+                };
+            }
+        }
+
+        /// <summary>
+        /// Get Customer Feedback by Id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public Object GetCustomerFeedbackById(int id)
+        {
+            using (HappyFarmPRG4Entities db = new HappyFarmPRG4Entities())
+            {
+                var customerFeedbacks = db.CustomerFeedbacks
+                    .Where(x => x.Id == id)
+                    .Select(x => new
+                    {
+                        x.Id,
+                        x.OrderId,
+                        x.Rating,
+                        x.Note
+                    })
+                    .FirstOrDefault();
+
+                return customerFeedbacks;
+            }
+        }
+    }
+}
