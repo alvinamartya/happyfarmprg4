bba226eee2f6dd98530611fd9130965a221ba7ca
Alvin Amartya <alvinamartya1@gmail.com>
2021-01-21 14:14:57 +0700

    Merge customer feedback

---
+++ HappyFarmProject/HappyFarmProjectWebAdmin/Models/ModelView/CustomerFeedbackModelView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HappyFarmProjectWebAdmin.Models
+{
+    public class CustomerFeedbackModelView
+    {
+        public int Id { get; set; }
+        public int OrderId { get; set; }
+        public double Rating { get; set; }
+        public string Note { get; set; }
+    }
+}
