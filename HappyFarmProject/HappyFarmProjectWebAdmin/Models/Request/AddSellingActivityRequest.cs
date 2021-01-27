0453fe8d47258c7b471d8003c504b734e500fb06
Alvin Amartya <alvinamartya1@gmail.com>
2021-01-27 22:26:09 +0700

    Fix Merging Change Password

---
+++ HappyFarmProject/HappyFarmProjectWebAdmin/Models/Request/AddSellingActivityRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HappyFarmProjectWebAdmin.Models
+{
+    public class AddSellingActivityRequest
+    {
+        public string SellingId { get; set; }
+        public int SellingStatusid { get; set; }
+        public int CreatedBy { get; set; }
+    }
+}
---
+++ HappyFarmProject/HappyFarmProjectWebAdmin/Models/Request/EditSellingActivityRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HappyFarmProjectWebAdmin.Models
+{
+    public class EditSellingActivityRequest
+    {
+        public int SellingStatusid { get; set; }
+        public int CreatedBy { get; set; }
+    }
+}
